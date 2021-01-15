8c385d4f7996f186771e37b6e8003fc94fe238eb
Yaroslav Furman <yaro330@gmail.com>
2021-01-16 00:24:42 +0300

    sched/tune: Disable forcing TA tasks to big cluster
    This only increases jitter from 0.07 ms to 0.2 ms and reduces
    big cores load from 25-30% to <5% while running the test.
    
    Change-Id: I8af55704cf65e8ccae7f185d7ec90547f9d4eedc
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -554,6 +554,25 @@ int schedtune_cpu_boost(int cpu)
 	return bg->boost_max;
 }
 
+static inline int schedtune_adj_ta(struct schedtune *st, struct task_struct *p)
+{
+	char name_buf[NAME_MAX + 1];
+	int adj = p->signal->oom_score_adj;
+
+	if (adj != 0)
+		return 0;
+
+	if (p->flags & PF_KTHREAD)
+		return 0;
+
+	cgroup_name(st->css.cgroup, name_buf, sizeof(name_buf));
+	if (!strncmp(name_buf, "top-app", strlen("top-app"))) {
+		return 1;
+	}
+
+	return 0;
+}
+
 int schedtune_task_boost(struct task_struct *p)
 {
 	struct schedtune *st;
@@ -747,7 +766,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		{ "background",	0, 0 },
 		{ "foreground",	0, 1 },
 		{ "rt",		0, 0 },
-		{ "top-app",	1, 1 },
+		{ "top-app",	0, 1 },
 	};
 	int i;
 
