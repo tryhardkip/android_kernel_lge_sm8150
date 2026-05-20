6221280f9d4da4606c374f540f039c1d57b5deee
xtrakari <nsrakip@gmail.com>
2026-05-20 10:18:02 +0800

    Revert "cpufreq: schedutil: Use a fixed shift for dvfs headroom"
    This reverts commit 7e3926bb8f9bb700c8ead5161033c5d0fa7eb65f.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -237,7 +237,7 @@ static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
          * capacity
          */
 	delta = capacity - util;
-	headroom = ((delta * delta) >> 12);
+	headroom = (delta * delta) / (4 * capacity);
 
 	/* 10% of capacity threshold */
     	min_util = capacity / 10;
