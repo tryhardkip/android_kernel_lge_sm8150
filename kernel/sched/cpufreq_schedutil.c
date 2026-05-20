8545a39458561e645703ff25e1cfe8524d98b039
xtrakari <nsrakip@gmail.com>
2026-05-20 10:17:58 +0800

    Revert "cpufreq: schedutil: Replace quadratic DVFS headroom with normalized cubic curve"
    This reverts commit 39ff88514fed9ecb3ea922a9a47262bb98b8bb08.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -235,10 +235,9 @@ static inline unsigned long sugov_apply_dvfs_headroom(unsigned long util,
 {
 	unsigned long delta, headroom;
 	unsigned long capped_util = min(util, capacity);
-	unsigned long delta_t = capacity - threshold;
 
 	delta = capacity - capped_util;
-	headroom = (delta * delta * delta * 5) / (delta_t * capacity * 16);
+	headroom = (delta * delta) >> 11;
 
 	if (capped_util < threshold)
 		headroom = (headroom * capped_util * capped_util) /
@@ -646,7 +645,7 @@ static void sugov_build_dvfs_headroom_lut(struct sugov_policy *sg_policy)
 
 	sg_policy->dvfs_capacity = capacity;
 
-	threshold = (capacity * 15) / 100;
+	threshold = (capacity >> 3) + (capacity >> 4);
 
 	for (util = 0; util <= SCHED_CAPACITY_SCALE; util++)
 		sg_policy->dvfs_headroom_lut[util] =
