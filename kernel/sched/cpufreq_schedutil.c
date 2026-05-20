8a812c181e8f127c7217e5dd28ff1d5e9910879e
xtrakari <nsrakip@gmail.com>
2026-05-20 10:18:08 +0800

    Revert "cpufreq: schedutil: Apply quadratic tapering to dvfs headroom"
    This reverts commit 0b20224833fc27a1f01a7b69a9b4f1ff26b8d2ea.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -224,24 +224,24 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	return cpufreq_driver_resolve_freq(policy, freq);
 }
 
-static const unsigned long factor[8] = { [0 ... 7] = 1280 - SCHED_CAPACITY_SCALE };
-
 static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 {
+	unsigned int sched_dvfs_headroom[8] = { [0 ... 7] = 1280 };
 	unsigned long capacity = capacity_orig_of(cpu);
-	unsigned long delta;
 	unsigned long headroom;
 
 	if (util >= capacity)
 		return util;
 
 	/*
-	 * Quadratic taper the boosting at the top end as these are expensive and
-	 * we don't need that much of a big headroom as we approach max capacity
+	 * Taper the boosting at the top end as these are expensive and
+	 * we don't need that much of a big headroom as we approach max
+	 * capacity
 	 */
-	delta = capacity - util;
-	headroom = (delta * delta * factor[cpu]) >> (SCHED_CAPACITY_SHIFT + 10);
-
+	headroom = (capacity - util);
+	/* formula: headroom * (1.X - 1) == headroom * 0.X */
+	headroom = headroom *
+		(sched_dvfs_headroom[cpu] - SCHED_CAPACITY_SCALE) >> SCHED_CAPACITY_SHIFT;
 	return util + headroom;
 }
 
