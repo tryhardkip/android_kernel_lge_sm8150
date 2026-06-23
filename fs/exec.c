9a41f6efc9e4f2c7762929196aeb858a69d11448
manipvlator <mr.morat14@gmail.com>
2026-06-23 12:54:22 +0700

    KernelSU: Backport SusFS v2.2.0
    Synced latest susfs from simonpunk
    
    Signed-off-by: manipvlator <mr.morat14@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1754,7 +1754,6 @@ static int __do_execve_file(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
-
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
 	 * set*uid() to execve() because too many poorly written programs
@@ -1930,6 +1929,7 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1957,6 +1957,7 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
