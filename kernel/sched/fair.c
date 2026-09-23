8998ccb7b596b8dfadac6f1332074f244af02d3e
tryhardkip <akipnsr@gmail.com>
2026-09-23 17:13:02 +0800

    sched/fair: remove dead cpu_in_sg() helper
    cpu_in_sg() was defined but never referenced. Being a static inline it
    produced no -Wunused-function warning, so it lingered as dead code.
    Remove it.
    
    Signed-off-by: tryhardkip <akipnsr@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6820,11 +6820,6 @@ next_cpu:
 	return 0;
 }
 
-static inline bool cpu_in_sg(struct sched_group *sg, int cpu)
-{
-	return cpu != -1 && cpumask_test_cpu(cpu, sched_group_span(sg));
-}
-
 #ifdef DEBUG_EENV_DECISIONS
 static void dump_eenv_debug(struct energy_env *eenv)
 {
